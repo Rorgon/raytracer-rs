740ef6d1f961c042bb6b285a6b4d4570300b2598
Sabau Florin <36925871+Rorgon@users.noreply.github.com>
2020-03-09 20:22:59 +0200

    Create sphere.rs

---
+++ scene/sphere.rs
@@ -0,0 +1,20 @@
+
+use crate::vec3::Vec3;
+use crate::ray::{Ray, Hittable};
+
+pub struct Sphere {
+    pub origin : Vec3,
+    pub radius : f64,
+    pub color : Vec3,
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray) -> bool {
+        let oc = ray.origin - self.origin;
+        let a = Vec3::dot(ray.direction,ray.direction);
+        let b = Vec3::dot(oc,ray.direction)*2.0;
+        let c = Vec3::dot(oc,oc) - self.radius*self.radius;
+        let delta = b*b-4.0*a*c;
+        delta>0.0
+    }
+}
